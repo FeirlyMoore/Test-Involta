1e76fc708a2850481fb30a6f5ba193562790d63e
REDFOX39 <daniil.fox1998@yandex.ru>
2022-01-25 04:05:16 +0300

    Dev 0.3 > Index stylization : Part 0.4

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,10 +1,70 @@
+
+// Настройки слайдера
+
 $(document).ready(function(){
   $('.news-block--js').slick({
     arrows: false,
     dots: false,
-    slidesToShow: 1
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: false,
+    responsive: [
+        // При разрешении до 10 000 пикселов
+        {
+          breakpoint: 10000,
+          settings: "unslick"
+        },
+        // При разрешении до 1039 пикселов
+        {
+          breakpoint: 1039,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            variableWidth: false
+          }
+        },
+        // При разрешении до 767 пикселов
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            variableWidth: false
+          }
+        },
+    ]
   });
 });
+
+// let clientWidth = document.body.clientWidth;
+
+// $('.news-block--js').slick({
+//     arrows: false,
+//     dots: false,
+//     infinite: true,
+//     slidesToScroll: 1,
+//     slidesToShow: 1
+//   });
+
+// function resizeTaleSlider(clientWidth) {
+//     if (clientWidth < 1040) {
+//       $('.news-block--js').slick({
+//         arrows: false,
+//         dots: false,
+//         infinite: true,
+//         slidesToScroll: 2,
+//       });
+//     } else {
+//       $('.news-block--js').slick("unslick");
+//     }
+//   }
+
+//   resizeTaleSlider(clientWidth);
+
+//   $(window).resize(function() {
+//     let clientWidth = document.body.clientWidth;
+//     resizeTaleSlider(clientWidth);
+//   });
 
 console.log(13123131)
 
