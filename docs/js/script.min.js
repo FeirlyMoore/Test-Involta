55105f355caa55d0e5a8d4d0efc9db6364fff49a
REDFOX39 <daniil.fox1998@yandex.ru>
2022-01-26 04:25:05 +0300

    Dev 0.3 > Index stylization : Part 0.6

--- docs/js/script.min.js
+++ docs/js/script.min.js
@@ -5,18 +5,11 @@ $(document).ready(function(){
   $('.news-block--js').slick({
     arrows: false,
     dots: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: false,
     responsive: [
         // При разрешении до 10 000 пикселов
         {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            variableWidth: false
-          }
+          breakpoint: 10000,
+          settings: "unslick"
         },
         // При разрешении до 1039 пикселов
         {
@@ -24,18 +17,62 @@ $(document).ready(function(){
           settings: {
             slidesToShow: 2,
             slidesToScroll: 1,
-            variableWidth: false
+            variableWidth: true
           }
         },
         // При разрешении до 767 пикселов
         {
-          breakpoint: 10000,
-          settings: "unslick"
-          
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            variableWidth: true
+          }
         },
     ]
   });
 });
+
+let width = $(window).width();
+
+$(window).resize(function(){
+  if( width < 1040) {
+    $(".slider").slick({
+      arrows: false,
+      dots: false,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      variableWidth: true,
+      responsive: [
+        // При разрешении до 767 пикселов
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            variableWidth: true
+          }
+        },
+    ]
+    });
+  }}
+);
+
+let NewsArticle = document.querySelectorAll('.news');
+let InnerWidth = window.innerWidth;
+
+  for (var i = 9; i <= NewsArticle.length - 1; i++) {
+  console.log(NewsArticle[i]);
+
+    if (InnerWidth > 1024 && InnerWidth < 1200) {
+      NewsArticle[i].classList.add('hidden');
+    } else {
+      NewsArticle[i].classList.remove('hidden');
+    }
+  }
+// window.addEventListener('resize', function(event) {
+// }, true);
+
 
 // let clientWidth = document.body.clientWidth;
 
