baa29bd9e040268b8fc381b838eeb3aa2fdc5640
REDFOX39 <daniil.fox1998@yandex.ru>
2022-01-29 00:02:44 +0300

    Dev 0.3 > Index stylization : Part 0.6

--- docs/js/script.min.js
+++ docs/js/script.min.js
@@ -1,6 +1,12 @@
 
 // Настройки слайдера
 
+// new Swiper('.news-block--js');
+
+// $(document).ready(function(){
+//   $('.news-block--js').slick();
+// });
+
 $(document).ready(function(){
   $('.news-block--js').slick({
     arrows: false,
@@ -33,77 +39,6 @@ $(document).ready(function(){
   });
 });
 
-let width = $(window).width();
-
-$(window).resize(function(){
-  if( width < 1040) {
-    $(".slider").slick({
-      arrows: false,
-      dots: false,
-      slidesToShow: 2,
-      slidesToScroll: 1,
-      variableWidth: true,
-      responsive: [
-        // При разрешении до 767 пикселов
-        {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            variableWidth: true
-          }
-        },
-    ]
-    });
-  }}
-);
-
-let NewsArticle = document.querySelectorAll('.news');
-let InnerWidth = window.innerWidth;
-
-  for (var i = 9; i <= NewsArticle.length - 1; i++) {
-  console.log(NewsArticle[i]);
-
-    if (InnerWidth > 1024 && InnerWidth < 1200) {
-      NewsArticle[i].classList.add('hidden');
-    } else {
-      NewsArticle[i].classList.remove('hidden');
-    }
-  }
-// window.addEventListener('resize', function(event) {
-// }, true);
-
-
-// let clientWidth = document.body.clientWidth;
-
-// $('.news-block--js').slick({
-//     arrows: false,
-//     dots: false,
-//     infinite: true,
-//     slidesToScroll: 1,
-//     slidesToShow: 1
-//   });
-
-// function resizeTaleSlider(clientWidth) {
-//     if (clientWidth < 1040) {
-//       $('.news-block--js').slick({
-//         arrows: false,
-//         dots: false,
-//         infinite: true,
-//         slidesToScroll: 2,
-//       });
-//     } else {
-//       $('.news-block--js').slick("unslick");
-//     }
-//   }
-
-//   resizeTaleSlider(clientWidth);
-
-//   $(window).resize(function() {
-//     let clientWidth = document.body.clientWidth;
-//     resizeTaleSlider(clientWidth);
-//   });
-
-console.log(13123131)
+console.log(12312313);
 
 //<!DOCTYPE Liky>
