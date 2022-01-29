a23884c92676b2cbacf8bbeeadf33811da2259a4
REDFOX39 <daniil.fox1998@yandex.ru>
2022-01-29 05:56:49 +0300

    Dev 1.0 > version : Alpha 1.0

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,16 @@
+window.addEventListener('resize', function(event) { // Функция, отслеживающая изменение размера окна
+  let InnerWidth = window.innerWidth; // Получаем размер окна
 
-// Настройки слайдера
+  if (InnerWidth < 1040) {
+    sliderInitialization(); // Запускаем функцию при размере окна меньшем, чем 1040 пикселов
+  }
 
-// new Swiper('.news-block--js');
+}, true);
 
-// $(document).ready(function(){
-//   $('.news-block--js').slick();
-// });
+document.addEventListener("DOMContentLoaded", sliderInitialization); // Запускаем функцию при загрузке страницы
 
-$(document).ready(function(){
+function sliderInitialization() { // Инициализация слайдера
+  // Настройки слайдера
   $('.news-block--js').slick({
     arrows: false,
     dots: false,
@@ -15,7 +18,7 @@ $(document).ready(function(){
         // При разрешении до 10 000 пикселов
         {
           breakpoint: 10000,
-          settings: "unslick"
+          settings: "unslick" // Отключаем слайдер
         },
         // При разрешении до 1039 пикселов
         {
@@ -37,8 +40,17 @@ $(document).ready(function(){
         },
     ]
   });
-});
+}
 
-console.log(12312313);
+
+let style = document.querySelector('style');
+let NewsCount = document.querySelectorAll('.news');
+// перекидываю... Ох, только сейчас понял, что SCSS скомпилируется раньше, чем инициализируется переменная. )))
+// Ахахах, да, идея была неплохая, надо будет взять на вооружение.
+style.innerHTML+= `
+:root {
+  --news-count: ${NewsCount.length};
+}
+`;
 
 //<!DOCTYPE Liky>
